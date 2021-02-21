4ea72f01568e923165991fe9d5d740338372f139
cjsta <18773138149@163.com>
2021-02-21 15:58:43 +0800

    + get handler

--- util/objectstream/objectstream.go
+++ util/objectstream/objectstream.go
@@ -8,18 +8,18 @@ import (
 
 type PutStream struct {
 	writer *io.PipeWriter
-	c chan error
+	c      chan error
 }
 
-func NewPutStream(server,object string) *PutStream {
-	reader,writer :=io.Pipe()
-	c :=make(chan error)
+func NewPutStream(server, object string) *PutStream {
+	reader, writer := io.Pipe()
+	c := make(chan error)
 	go func() {
-		request,_ :=http.NewRequest("PUT","htttp://"+server+"/objects/"+object,reader)
+		request, _ := http.NewRequest("PUT", "htttp://"+server+"/objects/"+object, reader)
 		client := http.Client{}
-		r,err := client.Do(request)
-		if err!=nil&&r.StatusCode != http.StatusOK{
-			err = fmt.Errorf("dataServer return htp code: %d",r.StatusCode)
+		r, err := client.Do(request)
+		if err != nil && r.StatusCode != http.StatusOK {
+			err = fmt.Errorf("dataServer return htp code: %d", r.StatusCode)
 		}
 		c <- err
 	}()
@@ -29,11 +29,35 @@ func NewPutStream(server,object string) *PutStream {
 	}
 }
 
-func (w *PutStream) Write (p []byte) (n int,err error) {
+func (w *PutStream) Write(p []byte) (n int, err error) {
 	return w.writer.Write(p)
 }
 
-func (w *PutStream) Close () error {
+func (w *PutStream) Close() error {
 	w.writer.Close()
 	return <-w.c
+}
+
+type GetStream struct {
+	reader io.Reader
+}
+
+func newGetStream(url string) (*GetStream, error) {
+	reader, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if reader.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("dataServer return http code %d", reader.StatusCode)
+	}
+	return &GetStream{reader: reader.Body}, nil
+}
+func NewGetStream(server, object string) (*GetStream, error) {
+	if server == "" || object == "" {
+		return nil, fmt.Errorf("invalid server %s object %s", server, object)
+	}
+	return newGetStream("http://" + server + "/objects/" + object)
+}
+func (read *GetStream) Read(p []byte) (n int, err error) {
+	return read.reader.Read(p)
 }
--- util/www/objects/objects.go
+++ util/www/objects/objects.go
@@ -5,64 +5,70 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
-	"storageApi/conf"
 	"storageApi/util/heartbeart"
 	"storageApi/util/objectstream"
+	"storageApi/util/www/locate"
 	"strings"
 )
 
-func Handler(w http.ResponseWriter,r *http.Request){
-	m :=r.Method
-	if m == http.MethodPut{
-		Put(w,r)
+func Handler(w http.ResponseWriter, r *http.Request) {
+	m := r.Method
+	if m == http.MethodPut {
+		put(w, r)
 		return
 	}
 	if m == http.MethodGet {
-		Get(w,r)
+		get(w, r)
 		return
 	}
 	w.WriteHeader(http.StatusMethodNotAllowed)
 }
 
-func Put(w http.ResponseWriter,r *http.Request)  {
-	object := strings.Split(r.URL.EscapedPath(),"/")[2]
-	c,err := storeObject(r.Body,object)
-	if err!=nil{
+func put(w http.ResponseWriter, r *http.Request) {
+	object := strings.Split(r.URL.EscapedPath(), "/")[2]
+	c, err := storeObject(r.Body, object)
+	if err != nil {
 		log.Println(err)
 	}
 	w.WriteHeader(c)
 }
 
-func storeObject(r io.Reader,object string) (int,error) {
-	stream,err := putStream(object)
-	if err!=nil{
-		return http.StatusServiceUnavailable,err
-	}
-	io.Copy(stream,r)
-	err = stream.Close()
-	if err!=nil{
-		return http.StatusInternalServerError,err
-	}
-	return http.StatusOK,nil
-}
-
-func putStream(object string) (*objectstream.PutStream,error) {
-	server := heartbeart.ChooseRandomDataServer()
-	if server == ""{
-		return nil ,fmt.Errorf("Cannot find any dataServer")
-	}
-	return objectstream.NewPutStream(server,object),nil
-}
-
-func Get(w http.ResponseWriter,r *http.Request)  {
-	f,err:=os.Open(conf.GetConfig().Env.Dir+"/objects/"+strings.Split(r.URL.EscapedPath(),"/")[2])
+func storeObject(r io.Reader, object string) (int, error) {
+	stream, err := putStream(object)
 	if err != nil {
-		log.Fatal(err)
+		return http.StatusServiceUnavailable, err
+	}
+	io.Copy(stream, r)
+	err = stream.Close()
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+	return http.StatusOK, nil
+}
+
+func putStream(object string) (*objectstream.PutStream, error) {
+	server := heartbeart.ChooseRandomDataServer()
+	if server == "" {
+		return nil, fmt.Errorf("Cannot find any dataServer")
+	}
+	return objectstream.NewPutStream(server, object), nil
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+	object := strings.Split(r.URL.EscapedPath(), "/")[2]
+	stream, err := getStream(object)
+	if err != nil {
+		log.Println(err)
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-	defer f.Close()
-	io.Copy(w,f)
+	io.Copy(w, stream)
 }
 
+func getStream(object string) (io.Reader, error) {
+	server := locate.Locate(object)
+	if server == "" {
+		return nil, fmt.Errorf("object %s locate fail", object)
+	}
+	return objectstream.NewGetStream(server, object)
+}
